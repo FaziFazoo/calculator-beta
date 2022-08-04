b7a9473d604410c6b31b034ee2a38930d87b344e
Fazil <fazilfarook006@gmail.com>
2022-08-04 23:45:57 +0530

    beta 1

---
+++ app.js
@@ -0,0 +1,14 @@
+function display(val) {
+  document.getElementById('result').value += val;
+  return val;
+}
+function clearScreen(){
+  document.getElementById('result').value = "";
+
+}
+function sol(){
+  let x = document.getElementById('result').value;
+  let y = eval(x);
+  document.getElementById('result').value = y;
+  return y;
+}
